d0b80ea3bc293899ea99ccfdeca22254af766b21
John Hancock <jhancoc4@fau.edu>
2014-09-07 15:28:12 -0400

    modify script for levy environment

--- assignment-2/run-classifiers.sh
+++ assignment-2/run-classifiers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-WEKA_JAR=/home/john/Documents/school/summer-2014/performance-modeling/weka-3-6-11/weka.jar
-DATA_FILE=/home/john/Documents/school/fall-2014/data-mining/assignments/assignment-1/Lymphoma96x4026.arff
-CLASSIFIER="weka.classifiers.trees.J48 -- -C 0.25 -M 2"
-COST_MATRIX="[0.0 22.5; 1.0 0.0]"
-java -cp  $WEKA_JAR weka.classifiers.meta.CostSensitiveClassifier -x 10 -t $DATA_FILE  -i -cost-matrix "$COST_MATRIX" -S 1 -W $CLASSIFIER
+WEKA_JAR=/home/john/Documents/school/fau/2014-summer/performance-modeling/weka-3-6-11/weka.jar
+DATA_FILE=/home/john/Documents/school/fau/2014-fall/data-mining/assignments/data-mining/assignment-1/Lymphoma96x4026.arff
+CLASSIFIER="weka.classifiers.meta.Bagging -- -P 100 -S 1 -I 10 -W weka.classifiers.trees.J48 -- -C 0.25 -M 2"
+COST_MATRIX="[0.0 1.0; 1.0 0.0]"
+java -cp  $WEKA_JAR weka.classifiers.meta.CostSensitiveClassifier  -t $DATA_FILE  -i -cost-matrix "$COST_MATRIX" -S 1 -W $CLASSIFIER
 
 
