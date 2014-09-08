2f53fe33a72e69c61457ea9b43b899cb6b562f09
John Hancock <jhancoc4@fau.edu>
2014-09-07 21:29:36 -0400

    script working with while loop

--- assignment-2/run-classifiers.sh
+++ assignment-2/run-classifiers.sh
@@ -2,7 +2,13 @@
 WEKA_JAR=/home/john/Documents/school/fau/2014-summer/performance-modeling/weka-3-6-11/weka.jar
 DATA_FILE=/home/john/Documents/school/fau/2014-fall/data-mining/assignments/data-mining/assignment-1/Lymphoma96x4026.arff
 CLASSIFIER="weka.classifiers.meta.Bagging -- -P 100 -S 1 -I 10 -W weka.classifiers.trees.J48 -- -C 0.25 -M 2"
-COST_MATRIX="[0.0 1.0; 1.0 0.0]"
-java -cp  $WEKA_JAR weka.classifiers.meta.CostSensitiveClassifier  -t $DATA_FILE  -i -cost-matrix "$COST_MATRIX" -S 1 -W $CLASSIFIER
+COST_MATRIX_FILE=/home/john/Documents/school/fau/2014-fall/data-mining/assignments/data-mining/assignment-2/cost-matrices.txt
+let NUM_COST_MATRICES=$(wc -l $COST_MATRIX_FILE)
+COUNTER=0
+while [  $COUNTER -lt $NUM_COST_MATRICES ]; do
+	COST_MATRIX=$(sed $i'q;d' cost-matrices.txt);
+	java -cp  $WEKA_JAR weka.classifiers.meta.CostSensitiveClassifier  -t $DATA_FILE  -i -cost-matrix "$COST_MATRIX" -S 1 -W $CLASSIFIER
+	let COUNTER=COUNTER+1;
+done
 
 
