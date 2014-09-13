0ba8b90fff815ea5e9e8867c674a4ce5521212eb
John Hancock <jhancoc4@fau.edu>
2014-09-13 11:07:25 -0400

    switch boosting/cost sensitive order of classifiers

--- assignment-2/run-classifiers.sh
+++ assignment-2/run-classifiers.sh
@@ -33,18 +33,15 @@ while [ $k -le $NUM_META_CLASSIFIERS ]; do
 	META_CLASSIFIER="$(sed $k'q;d' $META_CLASSIFIERS_FILE)"
 	let i=1;
 		while [  $i -le $NUM_CLASSIFIERS ];	do
-			CLASSIFIER="$META_CLASSIFIER $NUM_ITERATIONS"
-			CLASSIFIER="$CLASSIFIER $(sed $i'q;d' classifiers.txt)";
+			CLASSIFIER="$(sed $i'q;d' classifiers.txt)";
 			let j=1;
 			while [  $j -le $NUM_COST_MATRICES ];	do
 				COST_MATRIX=$(sed $j'q;d' cost-matrices.txt);
-				OUTPUT_FILE=$(echo $CLASSIFIER | sed 's/ //g' | sed 's/[-]\+/_/g');
+				OUTPUT_FILE=$(echo $META_CLASSIFIER | sed 's/;//g' | sed 's/\[//g' | sed 's/\]//g' | sed 's/ /_/g' | sed 's/"//g')
 				OUTPUT_FILE=$(echo $OUTPUT_FILE)$(echo $COST_MATRIX | sed 's/;//g' | sed 's/\[//g' | sed 's/\]//g' | sed 's/ /_/g' | sed 's/"//g')
+				OUTPUT_FILE=$(echo $OUTPUT_FILE)$(echo $CLASSIFIER | sed 's/ //g' | sed 's/[-]\+/_/g');
 				echo $OUTPUT_FILE;
-				java -cp  $WEKA_JAR \
-				weka.classifiers.meta.CostSensitiveClassifier \
-				-t $DATA_FILE  -i -cost-matrix "$COST_MATRIX" -S 1 -W $CLASSIFIER \
-				> $OUTPUT_FILE
+				java -cp  $WEKA_JAR $META_CLASSIFIER $NUM_ITERATIONS -t $DATA_FILE -W weka.classifiers.meta.CostSensitiveClassifier -- -cost-matrix "$COST_MATRIX" -S 1 $CLASSIFIER > $OUTPUT_FILE
 				let j=j+1;
 			done
 			let i=i+1;
