4b2e21b0c1816c3bc50383bc45738ecf83c4de05
john <jhancock1975@gmail.com>
2014-09-14 10:35:29 -0400

    use more printf and less cat in gnuplot command generator

--- assignment-2/generate-gnuplot-commands.sh
+++ assignment-2/generate-gnuplot-commands.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+rm -f $(find . -name "*gplt")
 pushd .
-PLOT_MID=$(pwd)/plot-middle.txt
-PLOT_END=$(pwd)/plot-end.txt
-PLOT_PRE=$(pwd)/plot-prefix.txt
 cd saved-output
 for dir in ./*/; do 
 	pushd .
@@ -11,11 +9,23 @@ for dir in ./*/; do
 	PLOT_CMD_FILE=$(echo $DIR_NAME.gplt)
 	FP_FILE=$(ls *fp*sorted)
 	FN_FILE=$(ls *fn*sorted)
-	cat $PLOT_PRE >> $PLOT_CMD_FILE
+	printf "set term postscript eps 22\n" >> $PLOT_CMD_FILE
+	printf "set key center top\n" >> $PLOT_CMD_FILE
+	printf "set xlabel 'Type II Error Cost'\n" >> $PLOT_CMD_FILE
+	printf "set ylabel 'Missclassifications'\n" >> $PLOT_CMD_FILE
+	printf "set linestyle 1 lt 2 lw 3\n" >> $PLOT_CMD_FILE
+	printf "set logscale x\n" >> $PLOT_CMD_FILE
+	printf "#set xrange [0.1:300]\n" >> $PLOT_CMD_FILE
+	printf "set key box linestyle 1 \n" >> $PLOT_CMD_FILE # dashed lines around box	
+	printf "set output '"$DIR_NAME".ps'\n" >> $PLOT_CMD_FILE # have to do every time
+	printf "plot " >> $PLOT_CMD_FILE
 	printf " "\'$FP_FILE\'" " >> $PLOT_CMD_FILE
-	cat $PLOT_MID >> $PLOT_CMD_FILE
-	printf " "\'$FN_FILE\'" " >> $PLOT_CMD_FILE
-	cat $PLOT_END >> $PLOT_CMD_FILE
+	HEADER=$(echo $dir | sed 's/\///g' | sed 's/\.//g' )
+	HEADER=${HEADER^^}
+	printf "title '"$HEADER" FPR ' with linespoints lw 3 pt 4, " >> $PLOT_CMD_FILE
+	printf "\'$FN_FILE\' "  >> $PLOT_CMD_FILE
+	printf "title '"$HEADER" FNR' with linespoints lw 3 pt 8\n" >> $PLOT_CMD_FILE
+	gnuplot < $PLOT_CMD_FILE
 	popd
 	done;
 popd
